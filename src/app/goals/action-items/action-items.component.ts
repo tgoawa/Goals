23b945328c7fa4926a4c44058dc66b53a1379d9c
sledgej <jonathan.sledge@schencksc.com>
2017-06-05 10:57:31 -0500

    generated action items component

---
+++ src/app/goals/action-items/action-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-action-items',
+  templateUrl: './action-items.component.html',
+  styleUrls: ['./action-items.component.scss']
+})
+export class ActionItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/goals/goals.module.ts
+++ src/app/goals/goals.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { GoalsRoutingModule } from './goals-routing.module';
 import { GoalsComponent } from './goals.component';
+import { ActionItemsComponent } from './action-items/action-items.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GoalsRoutingModule
   ],
-  declarations: [GoalsComponent]
+  declarations: [GoalsComponent, ActionItemsComponent]
 })
 export class GoalsModule { }
