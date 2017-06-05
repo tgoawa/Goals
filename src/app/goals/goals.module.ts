220962e94dd9055d51d651137e38ec555b152c04
sledgej <jonathan.sledge@schencksc.com>
2017-06-05 10:32:46 -0500

    generated base goal component for module

--- src/app/goals/goals.module.ts
+++ src/app/goals/goals.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GoalsRoutingModule } from './goals-routing.module';
+import { GoalsComponent } from './goals.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GoalsRoutingModule
   ],
-  declarations: []
+  declarations: [GoalsComponent]
 })
 export class GoalsModule { }
