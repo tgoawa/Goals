75efa4a96598045c727f1e50bc423654ac959a9e
sledgej <jonathan.sledge@schencksc.com>
2017-06-05 10:30:56 -0500

    generated goals module

---
+++ src/app/goals/goals.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GoalsRoutingModule } from './goals-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    GoalsRoutingModule
+  ],
+  declarations: []
+})
+export class GoalsModule { }
