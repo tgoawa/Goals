ac06399a8ca94b37a69c5d5a763a222fc2a57ea3
sledgej <jonathan.sledge@schencksc.com>
2017-06-06 09:08:00 -0500

    generated header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoalsModule } from './goals/goals.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
