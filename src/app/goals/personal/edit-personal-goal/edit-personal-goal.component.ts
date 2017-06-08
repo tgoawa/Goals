b3c1708e4db43665af3540fdbbf47a6a1f1794b7
sledgej <jonathan.sledge@schencksc.com>
2017-06-08 09:40:20 -0500

    added update service call to edit personal goal

--- src/app/goals/personal/edit-personal-goal/edit-personal-goal.component.ts
+++ src/app/goals/personal/edit-personal-goal/edit-personal-goal.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, AfterViewInit, Input, ViewChild, EventEmitter, Outpu
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 
+import { PersonalGoalService } from '../service/personal-goal.service';
+
 import { Goal, Action, Measurement, Support, Note } from '../../goal';
 
 @Component({
@@ -9,14 +11,14 @@ import { Goal, Action, Measurement, Support, Note } from '../../goal';
   templateUrl: './edit-personal-goal.component.html',
   styleUrls: ['./edit-personal-goal.component.scss']
 })
-export class EditPersonalGoalComponent implements OnInit {
+export class EditPersonalGoalComponent implements OnInit, AfterViewInit {
   @ViewChild('editModal') editModal: ModalDirective;
   @Input('personalGoal') personalGoal: Goal;
   @Output() modalClosed: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   editPersonalGoalForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private pgService: PersonalGoalService) { }
 
   ngOnInit() {
     this.editPersonalGoalForm = this.toFormGroup(this.personalGoal);
@@ -47,6 +49,16 @@ export class EditPersonalGoalComponent implements OnInit {
 
   modalIsClosed() {
     this.modalClosed.emit(true);
+  }
+
+  updateGoal(goal: Goal) {
+    this.pgService.updatePersonalGoal(goal)
+    .subscribe(data => {
+      this.pgService.getPersonalGoals(data.TeamMemberId);
+      this.hideModal();
+    }, error => {
+      console.log(error);
+    })
   }
 
   onSubmit(formValue: Goal) {
