be176d7689b0923707d8dd85a251e9a373352146
sledgej <jonathan.sledge@schencksc.com>
2017-06-19 12:58:27 -0500

    added meeting list component to routing and navigation

--- src/app/meetings/meetings-routing.module.ts
+++ src/app/meetings/meetings-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MeetingsComponent } from '../meetings/meetings.component';
+import { MeetingsListComponent } from '../meetings/non-shareholder/meetings-list/meetings-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MeetingsComponent,
     children: [
-      { }
+      {path: 'meeting-list', component: MeetingsListComponent }
     ]
   }
 ];
