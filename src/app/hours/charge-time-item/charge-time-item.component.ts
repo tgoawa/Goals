ff5d21b1840eda3ba9179662fc24a4f214c7815f
sledgej <jonathan.sledge@schencksc.com>
2017-07-05 10:39:25 -0500

    generated charge time component

---
+++ src/app/hours/charge-time-item/charge-time-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-charge-time-item',
+  templateUrl: './charge-time-item.component.html',
+  styleUrls: ['./charge-time-item.component.scss']
+})
+export class ChargeTimeItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/hours/hours.module.ts
+++ src/app/hours/hours.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HoursRoutingModule } from './hours-routing.module';
 import { HoursComponent } from './hours.component';
 import { HoursListComponent } from './hours-list/hours-list.component';
+import { ChargeTimeItemComponent } from './charge-time-item/charge-time-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HoursRoutingModule
   ],
-  declarations: [HoursComponent, HoursListComponent]
+  declarations: [HoursComponent, HoursListComponent, ChargeTimeItemComponent]
 })
 export class HoursModule { }
