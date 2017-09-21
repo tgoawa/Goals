cf65ec5cc9d6b32ea512d72ab811bd927bb3b429
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-09-21 10:24:29 -0500

    refactored functions into one loop

--- src/app/print/non-shareholder/meeting-questions/meeting.component.ts
+++ src/app/print/non-shareholder/meeting-questions/meeting.component.ts
@@ -13,22 +13,15 @@ export class MeetingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.replaceLineBreaks();
-    this.setNullToEmpty();
+    this.parseQuestionsArray();
   }
 
-  setNullToEmpty() {
-    for (let x = 0; x < this.questions.length; x++) {
-      if (this.questions[x].AnswerText === null) {
-        this.questions[x].AnswerText = '';
-      }
-    }
-  }
-
-  replaceLineBreaks() {
+  parseQuestionsArray() {
     for (let x = 0; x < this.questions.length; x++) {
       if (this.questions[x].AnswerText !== null) {
         this.questions[x].AnswerText = this.questions[x].AnswerText.replace(/(\r\n|\n|\r)/gm, '<br>');
+      } else {
+        this.questions[x].AnswerText = '';
       }
     }
   }
